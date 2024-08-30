ac0b87eb3cc67a8498790d867b65aa8b1e55632e
John <JonathanReeves@tuta.io>
2024-08-30 17:58:43 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   patch/bar.c
    	new file:   patch/bar.h
    new file:   patch/bar_alpha.c
    	new file:   patch/bar_alpha.h
    	new file:   patch/bar_indicators.c
    	new file:   patch/bar_indicators.h
    	new file:   patch/bar_ltsymbol.c
    	new file:   patch/bar_ltsymbol.h
    	new file:   patch/bar_status.c
    	new file:   patch/bar_status.h
    	new file:   patch/bar_systray.c
    	new file:   patch/bar_systray.h
    	new file:   patch/bar_tagicons.c
    	new file:   patch/bar_tagicons.h
    	new file:   patch/bar_tags.c
    	new file:   patch/bar_tags.h
    	new file:   patch/bar_winicon.c
    	new file:   patch/bar_winicon.h
    	new file:   patch/bar_wintitle.c
    	new file:   patch/bar_wintitle.h
    	new file:   patch/include.c
    	new file:   patch/include.h
    	new file:   patch/ipc/IPCClient.h
    	new file:   patch/ipc/dwm-msg.c
    	new file:   patch/ipc/yajl_dumps.h
    	new file:   patch/layout_facts.c
    	new file:   patch/layout_fibonacci.c
    	new file:   patch/layout_fibonacci.h
    	new file:   patch/layout_monocle.c
    	new file:   patch/layout_monocle.h
    	new file:   patch/layout_tile.c
    	new file:   patch/layout_tile.h
    	new file:   patch/roundedcorners.c
    	new file:   patch/roundedcorners.h
    	new file:   patch/swallow.c
    new file:   patch/swallow.h
    	new file:   patch/vanitygaps.c
    	new file:   patch/vanitygaps.h
    
     Untracked files:
    	dwm.desktop
    	flexipatch-finalizer.sh

---
+++ patch/include.h
@@ -0,0 +1,22 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_alpha.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_winicon.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "roundedcorners.h"
+#include "swallow.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
